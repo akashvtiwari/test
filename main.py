3e71c7a47e149eb242016426bf069efe92006a9c
akashvtiwari <akashvtiwari777@gmail.com>
2023-01-03 10:48:11 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,220 @@
+# # first_name = "akash"
+# # last_name = "tiwari"
+# # full_name = f"{first_name} {last_name}"
+# # message = f"Hello, {full_name.title()}!"
+# # print(message)
+
+
+# # harsh = 'jayesh'
+# # rating = '10/10'
+# # info = f"harsh name is {harsh} and rating is {rating}"
+# # print(info.title)
+
+# # print('language:\n\tpython\n\tc\n\tjavascript')
+
+# # favourite_language = 'python'
+# # print(favourite_language)
+# # # 'python'
+# # favourite_language.rstrip()
+# # # 'python'
+# # print(favourite_language)
+# # # 'python'
+
+# print(2+3)
+# print(3-2)
+# print(3*2)
+# print(3/2)
+# print(3**2)
+# print(3**3)
+# print(10**6)
+
+#name,name1,name2 = 'akash' ,'akash1' ,'akash2'
+#print(name1) 
+
+#shoping = ['cloth','watch','bag']
+#print(shoping)
+
+
+#bicycles =['trek','cannondable','readline','specialized']
+#print(bicycles[-1])
+
+# motorcycles = ['honda', 'yamaha', 'suzuki']
+# print(motorcycles)
+# motorcycles[2] = 'ducati'
+# print(motorcycles)
+
+#motorcycles = ['honda', 'yamaha', 'suzuki']
+#motorcycles.insert(0, 'ducati')
+#print(motorcycles)
+
+#motorcycles = ['honda', 'yamaha', 'suzuki']
+#print(motorcycles)
+#del motorcycles[0]
+#print(motorcycles)
+
+#motorcycles = ['honda', 'yamaha']
+#print(motorcycles)
+#popped_motorcycle = motorcycles.pop()
+#print(motorcycles)
+#print(popped_motorcycle)
+
+#motorcycles = ['honda', 'yamaha', 'suzuki']
+#first_owned = motorcycles.pop(0)
+#print(f"The first motorcycle I owned was a {first_owned.title()}.")
+
+#motorcycles = ['honda', 'yamaha', 'suzuki'] 
+#last_owned = motorcycles.pop(0) 
+#print(f"The last motorcycle I owned was a {last_owned.title()}.")
+
+#motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+#print(motorcycles)
+#motorcycles.remove('ducati')
+#print(motorcycles)
+
+#motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati'] 
+#print(motorcycles)
+#too_expensive = 'ducati'
+#motorcycles.remove(too_expensive)
+#print(motorcycles)
+#print(f"\nA {too_expensive.title()} is too expensive for me.")
+
+#cars = ['bmw','audi','toyota','subaru']
+#print(len(cars))
+
+#magicians =['alice','david','carolina','akash']
+#for magician in magicians:
+#  print(magicians)
+
+#animes = ['naruto','onepiece','dbz']
+#for akash in animes:
+# print(akash)  
+
+#magicians = ['alice','david','carolina']
+#for magician in magicians:
+#    print(f'{magician.title()},that was a great trick!')
+#    print(f'i cant wait to see your next trick,{magician.title()}.\n')
+#print('thank you,everyone.that was a great magic show!')    
+
+#for value in range(1,5):
+#    print(value)
+
+# squares = []
+# for value in range(1,11):
+#    squares.append(value**2)
+# print(squares)
+
+# squares = []
+# for value in range(1, 11):
+#   square = value ** 2
+#   squares.append(square)
+# print(squares)
+
+#magicians = ['alice', 'david', 'carolina']
+#for magician in magicians:
+#  print(magician)
+
+#bikes = ['honda','hero','shine']
+#for bike in bikes:
+#    print(bikes) 
+
+# cars = ['audi', 'bmw', 'subaru', 'toyota']
+# for car in cars:
+#     if car == 'bmw':
+#         print(car.upper())
+# else:
+#     print(car.title())
+
+# requested_topping = 'mushrooms'
+# if requested_topping != 'anchovies':
+#     print("Hold the anchovies!")
+
+# answer = 17
+# if answer != 42:
+#     print("That is not the correct answer. Please try again!")
+
+# magicians = ['alice', 'david', 'carolina']
+# for magician in magicians:
+#   print(f"{magician.title()}, that was a great trick!")
+#   print(f"I can't wait to see your next trick, {magician.title()}.\n")
+# print("Thank you, everyone. That was a great magic show!\n")
+
+# magicians = ['alice', 'david', 'carolina']
+# for magician in magicians:
+#   print(f"{magician.title()}, that was a great trick!")
+# print(f"I can't wait to see your next trick, {magician.title()}.\n")
+
+#for value in range(1, 5):
+#   print(value)
+
+# akash = list(range(1, 6))
+# print(akash)
+
+#even_numbers = list(range(1, 11, 1))
+#print(even_numbers)
+
+# digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+# min(digits)
+#  0
+# max(digits)
+#  9
+# sum(digits)
+#  45
+
+# squares = [value**2 for value in range(1, 11)]
+# print(squares)
+
+# players = ['charles', 'martina', 'michael', 'florence', 'AZAD']
+# print(players[0:3])
+# print(players[1:4])
+# print(players[:4])
+# print(players[2:])
+# print(players[-3:])
+
+# my_foods = ['pizza','falafel','carrot cake']
+# friend_foods = my_foods[:]
+# my_foods.append('cannoli')
+# friend_foods.append('ice cream')
+# print('My favourite foods are:')
+# print(my_foods)
+# print('\nMy friends favourite foods are:')
+# print(friend_foods)
+
+# dimension = 200,50
+# print(dimension[0])
+# print(dimension[1])
+
+# age_0 = 22 
+# age_1 = 18 
+# age_0 >= 21 and age_1 >= 21 
+# age_1 = 22 
+# age_0 >= 21 and age_1 >= 21 
+
+# age = 20
+# if age >= 18:
+#     print("You are old enough to vote!")
+#     print("Have you registered to vote yet?")
+# else:
+#     print("Sorry, you are too young to vote.")
+#     print("Please register to vote as soon as you turn 18!")
+
+# age = 12
+# if age < 4:
+#     print("Your admission cost is $0.")
+# elif age < 18:
+#     print("Your admission cost is $25.")
+# else:
+#     print("Your admission cost is $40.")
+
+# requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+# for requested_topping in requested_toppings:
+#     print(f"Adding {requested_topping}.")
+# print("\nFinished making your pizza!")
+
+# dirs = {'name':'akki'}
+# print(dirs['name'])
+
+alien_0 ={'colour': 'green', 'points': 5}
+print(alien_0)
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
